41b6b3b38f9d75738cfa3cf278bb0e6d4d033756
ahhridoy <ahhridoyy28@gmail.com>
2021-12-18 23:43:42 +0600

    footer added2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import ProductDetails from "./components/ProductDetails";
 import ProductList from "./components/ProductList";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
 function App() {
